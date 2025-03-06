7cb16e33bbee7754971733dbad412e4f6be0726b
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-06 11:21:07 +0100

    upd

--- linux/src/api/mod.rs
+++ linux/src/api/mod.rs
@@ -1,12 +1,20 @@
 mod ffi;
 mod web_auth;
 
+use flutter_rust_bridge::frb;
+
 #[frb]
-pub fn web_authentication_login(request: ffi::AuthRequestData) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
+pub fn web_authentication_login(
+    request: ffi::AuthRequestData,
+) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
     ffi::web_authentication_login(request)
 }
 
 #[frb]
-pub fn web_authentication_logout(domain: String, client_id: String, return_to: Option<String>) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
+pub fn web_authentication_logout(
+    domain: String,
+    client_id: String,
+    return_to: Option<String>,
+) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
     ffi::web_authentication_logout(domain, client_id, return_to)
 }
