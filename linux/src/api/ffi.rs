f62ecf1f3907c19d0d2e1510028b40865713eac6
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-06 11:27:19 +0100

    upd

--- linux/src/api/ffi.rs
+++ linux/src/api/ffi.rs
@@ -1,20 +1,26 @@
 use flutter_rust_bridge::ZeroCopyBuffer;
 use serde::{Deserialize, Serialize};
 
+use flutter_rust_bridge::frb;
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct AuthRequestData {}
 
-pub fn web_authentication_login(request: AuthRequestData) -> ZeroCopyBuffer<Vec<u8>> {
+#[frb]
+pub fn web_authentication_login(
+    request: AuthRequestData,
+) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
     println!("{:?}", request);
     let response = vec![];
     ZeroCopyBuffer(response)
 }
 
+#[frb]
 pub fn web_authentication_logout(
     domain: String,
     client_id: String,
     return_to: Option<String>,
-) -> ZeroCopyBuffer<Vec<u8>> {
+) -> flutter_rust_bridge::ZeroCopyBuffer<Vec<u8>> {
     let response = vec![];
     ZeroCopyBuffer(response)
 }
